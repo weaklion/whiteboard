bcbbd4a1d07ed35161253872a19e274c81e44c50
gyu han lee <weaklion@naver.com>
2026-01-16 17:16:26 +0900

    📦(add): server,client 분리, socket.io 추가 진행중

---
+++ server/src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+
+app.set("port", process.env.PORT || 3001);
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const server = app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번에서 대기중");
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  })
+
+  socket.on("error", (error) => {
+    console.log("User error", error);
+  })
+
+  socket.on("reply", (data) => {
+    console.log("User reply", data);
+  })   
+
+  socket.emit("welcome", "Hello, Welcome to the chat!");
+
+  console.log("User connected");
+});
